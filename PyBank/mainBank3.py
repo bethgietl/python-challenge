ee2b07229d8c783c822d981c2dcf7e3182106c5f
bethgietl <bfave19@gmail.com>
2021-03-26 09:45:15 -0600

    in PyBank chanllenge go the previous net to print the line above

--- PyBank/mainBank3.py
+++ PyBank/mainBank3.py
@@ -19,22 +19,41 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 txtpath = os.path.join("Analysis", "Financial_Analysis.txt")
 
 months = []
+total_profit = 0
 
+#       1. Open the path, CSV reader specifies delimiter and variable that holds contents, Read the header and first rows
 with open(csvpath, newline='') as csvfile:
-        #CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-        # Read the header row first (skip this step if there is now header)
+
     csv_header = next(csvreader)   
     first_data = next(csvreader)
 
+#       2. to find the total months start with appending the months list to col. 0 (Months) then use the len funtion to find the length of the month list    
     for column in csvreader:
-        months.append(column[1])
+        months.append(column[0])
     
     total_months = (len(months))+1
 
-print(total_months)
+    #print(total_months)
 
+#       3. net the total amount of "Profit/Losses" over the entire period sum
 
+    total_profit += int(first_data[1]) 
+    previous_net = int(first_data[1])
+
+    for row in csvreader:
+        total_months = total_months + 1 #4
+        
+        total_profit += int(row[1]) #867884 + 984655 + 322013 + -69417
+        net_profit_change = int(row[1]) - previous_net #-69417 - 322013
+        changes_in_profit.append (net_profit_change) #[116771, -662642, -391430]
+        previous_net = int(row[1]) #-69417
+
+print(previous_net)
+        
+        #if net_profit_change > greatest_increase[1]:  #["1900", 0]
+            #greatest_increase[0] = row[0] #[Feb-10, 0]
+            #greatest_increase[1] = net_profit_change #[Feb-10,116771]
 
 
 
